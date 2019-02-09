05ddf040f64ae7c438ec804e6a4afe0274beee75
Ozan Mora <ozanmora@users.noreply.github.com>
2019-02-09 11:06:37 +0300

    Update Log_query.php
    Fixed bug when debug log was active and no database operation was performed.

--- application/hooks/Log_query.php
+++ application/hooks/Log_query.php
@@ -61,48 +61,50 @@ class Log_Query
         $filepath = $this->_log_path.'query-'.date('Y-m-d').'.'.$this->_file_ext;
         $message = '';
         
-        if ( ! file_exists($filepath)) {
-            $newfile = true;
-            // Only add protection to php files
-            if ($this->_file_ext === 'php') {
-                $message .= "<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>\n\n";
-            }
-        }
-
-        if ( ! $fp = @fopen($filepath, 'ab')) {
-            return false;
-        }
-
-        flock($fp, LOCK_EX);
-
-        // Instantiating DateTime with microseconds appended to initial date is needed for proper support of this format
-        if (strpos($this->_date_fmt, 'u') !== false) {
-            $microtime_full = microtime(true);
-            $microtime_short = sprintf("%06d", ($microtime_full - floor($microtime_full)) * 1000000);
-            $date = new DateTime(date('Y-m-d H:i:s.'.$microtime_short, $microtime_full));
-            $date = $date->format($this->_date_fmt);
-        } else {
-            $date = date($this->_date_fmt);
-        }
-
         $times = $this->CI->db->query_times;
-        foreach ($this->CI->db->queries as $key => $query) {
-            $message .= 'QUERY' . ' - ' . $date . ' --> ' . round($times[$key], 4) . ' | ' . str_replace(array("\n", "\n\r", "\r", PHP_EOL), " ", $query) . "\n";
-        }
-
-        for ($written = 0, $length = self::strlen($message); $written < $length; $written += $result) {
-            if (($result = fwrite($fp, self::substr($message, $written))) === false) {
-                break;
+        if (!empty($times)) {
+            if ( ! file_exists($filepath)) {
+                $newfile = true;
+                // Only add protection to php files
+                if ($this->_file_ext === 'php') {
+                    $message .= "<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>\n\n";
+                }
             }
-        }
 
-        flock($fp, LOCK_UN);
-        fclose($fp);
-        
-        if (isset($newfile) && $newfile === true) {
-            chmod($filepath, $this->_file_permissions);
+            if ( ! $fp = @fopen($filepath, 'ab')) {
+                return false;
+            }
+
+            flock($fp, LOCK_EX);
+
+            // Instantiating DateTime with microseconds appended to initial date is needed for proper support of this format
+            if (strpos($this->_date_fmt, 'u') !== false) {
+                $microtime_full = microtime(true);
+                $microtime_short = sprintf("%06d", ($microtime_full - floor($microtime_full)) * 1000000);
+                $date = new DateTime(date('Y-m-d H:i:s.'.$microtime_short, $microtime_full));
+                $date = $date->format($this->_date_fmt);
+            } else {
+                $date = date($this->_date_fmt);
+            }
+
+            foreach ($this->CI->db->queries as $key => $query) {
+                $message .= 'QUERY' . ' - ' . $date . ' --> ' . round($times[$key], 4) . ' | ' . str_replace(array("\n", "\n\r", "\r", PHP_EOL), " ", $query) . "\n";
+            }
+
+            for ($written = 0, $length = self::strlen($message); $written < $length; $written += $result) {
+                if (($result = fwrite($fp, self::substr($message, $written))) === false) {
+                    break;
+                }
+            }
+
+            flock($fp, LOCK_UN);
+            fclose($fp);
+            
+            if (isset($newfile) && $newfile === true) {
+                chmod($filepath, $this->_file_permissions);
+            }
+            return is_int($result);
         }
-        return is_int($result);
     }
 
     // --------------------------------------------------------------------
